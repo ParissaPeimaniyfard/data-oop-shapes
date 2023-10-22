00f36ccaabf292e863bc9b75455f0b2df75cb8a3
Parissa Peimaniyfard <zahra.pmf@gmail.com>
2023-10-22 23:11:07 +0200

    second solve

--- shape.py
+++ shape.py
@@ -1,56 +1,70 @@
-class shape:
+'''test classes'''
+class Shape:
+    """ this is the shape class """
     def __init__(self, color, name):
-        self.shapeName = name
-        self.shapeColor = color
-    def say_name(self):
-        print ("My name is", self.shapeName)
+        """ set initial values """
+        self.shape_name = name
+        self.shape_color = color
 
-test= shape ("blue","xyz")
+    def say_name(self):
+        """ Print the value """
+        print ("My name is", self.shape_name)
+
+test= Shape ("blue","xyz")
 test.say_name()
 
-class Rectangle (shape):
+class Rectangle (Shape):
+    ''' this is rectangle class'''
     def __init__(self, color, name, width, height):
-        shape.__init__(self,color,name)
+        ''' set parameters'''
+        Shape.__init__(self,color,name)
         #self.recName= name
-        self.recWidth= width
-        self.recHeigh= height
+        self.rec_width= width
+        self.rec_height= height
 
     def say_name(self):
-        shape.say_name(self)
-        print ("My name is", self.shapeName, "and I am a rectangle")
+        ''' print the name'''
+        Shape.say_name(self)
+        print ("My name is", self.shape_name, "and I am a rectangle")
 
-    def areaRec(self):
-        self.recArea=self.recHeigh*self.recWidth
-        print ("Area:", self.recArea)
+    def area_rec(self):
+        ''' calculate rectangle area'''
+        self.rec_area=self.rec_height*self.rec_width
+        print ("Area:", self.rec_area)
 
     def perimeter(self):
-        self.recPerimeter= (self.recHeigh+self.recWidth)*2
-        print ("Perimeter:", self.recPerimeter)
+        ''' calculate rectangle perimeter'''
+        self.rec_perimeter= (self.rec_height+self.rec_width)*2
+        print ("Perimeter:", self.rec_perimeter)
 
 test2= Rectangle ("red","Rei", 8, 9)
 test2.say_name()
-test2.areaRec()
+test2.area_rec()
 test2.perimeter()
 
-class Circle (shape):
+class Circle (Shape):
+    ''' circle class'''
     def __init__(self, color, name, radius):
-        shape.__init__(self,color,name)
+        ''' set the parameters'''
+        Shape.__init__(self,color,name)
         #self.circName= name
-        self.radiusCircle= radius
+        self.radius_circle = radius
 
     def say_name(self):
-        shape.say_name(self)
-        print ("My name is", self.shapeName, "and I am a circle")
+        '''print the name'''
+        Shape.say_name(self)
+        print ("My name is", self.shape_name, "and I am a circle")
 
-    def areaCircle(self):
-        self.Cirarea= 3.14 * self.radiusCircle * self.radiusCircle
-        print ("Area:", self.Cirarea)
+    def area_circle(self):
+        '''calculate circle area'''
+        self.cirarea= 3.14 * self.radius_circle * self.radius_circle
+        print ("Area:", self.cirarea)
 
-    def perimeterCircle(self):
-        self.perimeterCircle= 2 * 3.14 * self.radiusCircle
-        print ("Perimeter:", self.perimeterCircle)
+    def perimeter_circle(self):
+        ''' calculate circle perimeter'''
+        return 2 * 3.14 * self.radius_circle
 
-test3= Circle("yellow","klot", 6)
+test3= Circle("yellow","klot", 3)
 test3.say_name()
-test3.areaCircle()
-test3.perimeterCircle()
+test3.area_circle()
+test3.perimeter_circle()
